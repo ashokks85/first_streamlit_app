b62305b3b0520f2da6b921c0e3398b44d164ae24
ashokks85 <ashokks.eie@gmail.com>
2022-08-27 12:33:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale and Spinach')
-streamlit.text('Hard Boiled Eggs')
+streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale and Spinach')
+streamlit.text(' 🐔Hard Boiled Eggs')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
