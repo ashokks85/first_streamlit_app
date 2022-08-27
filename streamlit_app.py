809876197ce40ea13b931c13d8f68a649be97443
ashokks85 <ashokks.eie@gmail.com>
2022-08-27 12:30:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale and Spinach')
+streamlit.text('Hard Boiled Eggs')
